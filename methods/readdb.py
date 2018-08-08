e7fe4cc1cfaf21694c3152c8c84eabb5c6e55b02
stony <24898323+hailStony2017@users.noreply.github.com>
2018-08-08 00:46:13 -0400

    debg

--- methods/readdb.py
+++ methods/readdb.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env Python
 # coding=utf-8
 
-from .db import *
+# from .db import *
 
 
 def select_table(table, column, condition, value):
-    sql = "select {} from {} where {} = '{}'".format(column, table, condition, value)
-    cur.execute(sql)
-    lines = cur.fetchall()
+    # sql = "select {} from {} where {} = '{}'".format(column, table, condition, value)
+    # cur.execute(sql)
+    # lines = cur.fetchall()
+    lines = ((1, 'qq', '123123', 'qq@gmail.com'),)
     return lines
 
 
 def select_columns(table, column):
-    sql = "select {} from {}".format(column, table)
-    cur.execute(sql)
-    lines = cur.fetchall()
+    # sql = "select {} from {}".format(column, table)
+    # cur.execute(sql)
+    # lines = cur.fetchall()
+    lines = ((1, 'qq', '123123', 'qq@gmail.com'),)
     return lines

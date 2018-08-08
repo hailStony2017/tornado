6a38d095a31fdc4b27450239791ca7afceffc796
stony <24898323+hailStony2017@users.noreply.github.com>
2018-08-08 01:02:04 -0400

    test case

--- server.py
+++ server.py
@@ -2,21 +2,24 @@
 # coding=utf-8
 
 import tornado.ioloop
-import tornado.options
 import tornado.httpserver
+import tornado.web
 
 from application import application
 
-from tornado.options import define, options
-
-define("port", default = 8000, help = "run on the given port", type = int)
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World")
 
 def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
+    application = tornado.web.Application([
+        (r"/", TestHandler),
+    ])
 
-    print("Development server is running at http://127.0.0.1:%s" % options.port)
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = 5000
+    http_server.listen(port)
+    print("Development server is running at http://127.0.0.1:%s" % port)
     print("Quit the server with Control-C")
 
     tornado.ioloop.IOLoop.instance().start()
